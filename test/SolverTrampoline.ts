dd7ec1d4cfc33bc7df0df93c148c3111d52411b1
Nicholas Rodrigues Lordello <nick@cow.fi>
2022-12-13 10:47:18 +0000

    Implement EIP-712 Message Hashing

--- test/SolverTrampoline.ts
+++ test/SolverTrampoline.ts
@@ -7,14 +7,51 @@ describe("SolverTrampoline", function () {
     const SolverTrampoline = await ethers.getContractFactory("SolverTrampoline");
     const solverTrampoline = await SolverTrampoline.deploy();
 
-    return { solverTrampoline };
+    const { chainId } = await ethers.provider.getNetwork();
+    const domain = {
+      chainId,
+      verifyingContract: solverTrampoline.address,
+    };
+
+    return { solverTrampoline, domain };
   }
 
   describe("Deployment", function () {
     it("Should have an address", async function () {
       const { solverTrampoline } = await loadFixture(fixture);
 
-      expect(solverTrampoline.address).to.not.equal(ethers.constants.AddressZero);
+      expect(solverTrampoline.address)
+        .to.not.equal(ethers.constants.AddressZero);
+    });
+  });
+
+  describe("settlementMessage", function () {
+    it("Should have a well defined domain separator", async function () {
+      const { solverTrampoline, domain } = await loadFixture(fixture);
+
+      expect(await solverTrampoline.domainSeparator())
+        .to.equal(ethers.utils._TypedDataEncoder.hashDomain(domain));
+    });
+
+    it("Should compute a EIP-712 message for signing", async function () {
+      const { solverTrampoline, domain } = await loadFixture(fixture);
+      const settlement = "0x01020304";
+      const nonce = 42;
+
+      expect(await solverTrampoline.settlementMessage(settlement, nonce))
+        .to.equal(
+          ethers.utils._TypedDataEncoder.hash(domain, EIP712_TYPES, {
+            settlement,
+            nonce,
+          }),
+        );
     });
   });
 });
+
+const EIP712_TYPES = {
+  Settlement: [
+    { name: "settlement", type: "bytes" },
+    { name: "nonce", type: "uint256" },
+  ],
+};
