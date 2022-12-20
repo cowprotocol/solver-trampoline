d230ce45a8eb272f5e245c61c9bd4ecc4381234e
Nicholas Rodrigues Lordello <nick@cow.fi>
2022-12-20 22:52:05 +0100

    Add Solver Trampoline Gas Benchmarking (#6)
    This PR adds solver trampoline gas benchmarking so we can make informed
    decisions about optimizations.
    
    ### Test Plan
    
    ```
    % npm run -s bench
    gas used: 16507
    ```

---
+++ scripts/bench.ts
@@ -0,0 +1,55 @@
+import { ethers } from "hardhat";
+
+async function main() {
+  const [solver] = await ethers.getSigners();
+
+  const TestSettlement = await ethers.getContractFactory("TestSettlement");
+  const settlementContract = await TestSettlement.deploy(solver.address);
+
+  const SolverTrampoline = await ethers.getContractFactory("SolverTrampoline");
+  const solverTrampoline = await SolverTrampoline.deploy(settlementContract.address);
+
+  await solverTrampoline.deployed();
+
+  const { chainId } = await ethers.provider.getNetwork();
+  async function trampolineEmptyTestSettlement() {
+    const emptySettlement = "0x";
+    const nonce = await solverTrampoline.nonces(solver.address);
+    const signature = await solver._signTypedData(
+      {
+        chainId,
+        verifyingContract: solverTrampoline.address,
+      },
+      {
+        Settlement: [
+          { name: "settlement", type: "bytes" },
+          { name: "nonce", type: "uint256" },
+        ],
+      },
+      {
+        settlement: emptySettlement,
+        nonce,
+      },
+    );
+
+    const { r, s, v } = await ethers.utils.splitSignature(signature);
+    return await solverTrampoline.settle(emptySettlement, nonce, r, s, v);
+  }
+
+  // Ensure that the solver already has executed a trampolined settlement in
+  // order for its nonce to be non-zero.
+  const firstSettlement = await trampolineEmptyTestSettlement();
+  await firstSettlement.wait();
+
+  const trampolinedSettlement = await trampolineEmptyTestSettlement();
+  const { gasUsed: trampolineGasUsed } = await trampolinedSettlement.wait();
+
+  const directSettlement = await settlementContract.fallback();
+  const { gasUsed: directGasUsed } = await directSettlement.wait();
+  console.log(`gas used: ${trampolineGasUsed.sub(directGasUsed)}`);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
